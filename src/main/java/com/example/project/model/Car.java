7b1aaaa7a795c4e9b59aeeaa35b1e1e316750e1b
Dominik Prokop <dominikprokopcz@gmail.com>
2024-10-02 11:30:12 +0200

    Car model implemented

--- src/main/java/com/example/project/model/Car.java
+++ src/main/java/com/example/project/model/Car.java
@@ -1,4 +1,51 @@
 package com.example.project.model;
 
 public class Car {
+    private String spz;
+    private String color;
+    private int numberOfSeats;
+    private float tankCapacity;
+
+    public Car() {
+
+    }
+
+    public Car(String spz, String color, int numberOfSeats, float tankCapacity) {
+        this.spz = spz;
+        this.color = color;
+        this.tankCapacity = tankCapacity;
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public String getSpz() {
+        return spz;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public float getTankCapacity() {
+        return tankCapacity;
+    }
+
+    public void setSpz(String spz) {
+        this.spz = spz;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public void setTankCapacity(float tankCapacity) {
+        this.tankCapacity = tankCapacity;
+    }
 }
